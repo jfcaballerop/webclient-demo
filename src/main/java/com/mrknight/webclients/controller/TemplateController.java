c858c8c8846e0b6396e0ba019d10e89d46021226
jfcaballerop <jfcaballerop@gmail.com>
2023-05-27 21:28:38 +0200

    add response

--- src/main/java/com/mrknight/webclients/controller/TemplateController.java
+++ src/main/java/com/mrknight/webclients/controller/TemplateController.java
@@ -10,6 +10,10 @@ public class TemplateController {
   @RequestMapping("/helloWorld")
   public String hello(Model modelo) {
 
+    /*
+     * Habría que añadir la llamada con WebClient para poder mandar la info a la Web
+     */
+
     modelo.addAttribute("mensaje", "Hello World!");
 
     return "helloWorld";
