a22a90915ef39c26833702b99482ebf0f36442ec
jfcaballerop <jfcaballerop@gmail.com>
2023-05-31 22:33:59 +0200

    add service

---
+++ src/main/java/com/mrknight/webclients/configuration/Constants.java
@@ -0,0 +1,10 @@
+package com.mrknight.webclients.configuration;
+
+public final class Constants {
+
+  public static final String URL_STRING = "https://my-json-server.typicode.com/jfcaballerop/fantasy-demo";
+  public static final String POSTS = "/posts";
+  public static final String COMMENTS = "/comments";
+  public static final String ALL = "/db";
+
+}
---
+++ src/main/java/com/mrknight/webclients/services/MyJsonServerService.java
@@ -0,0 +1,8 @@
+package com.mrknight.webclients.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyJsonServerService {
+
+}
